4e322f367fa68981ec31129a02c0ec2782307e09
GoutteDooo <goudal.donovan@live.fr>
2025-05-13 11:34:05 +0200

    2025-05-13 11:34:05

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -18,6 +18,7 @@ def add(request):
     if form.is_valid():
       task = form.cleaned_data["task"]
       tasks.append(task)
+      return HttpResponseRedirect(reverse("tasks:index"))
     else:
       return render(request, "tasks/add.html", {
         "form": form
