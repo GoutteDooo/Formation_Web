1864e8709483ca35c2369b6275a12ffebe5c1046
GoutteDooo <goudal.donovan@live.fr>
2025-05-15 10:54:00 +0200

    2025-05-15 10:54:00

--- lecture4/airline/flights/admin.py
+++ lecture4/airline/flights/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Airport, Flight, Passenger
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+
 admin.site.register(Airport)
 admin.site.register(Flight)
 admin.site.register(Passenger)
