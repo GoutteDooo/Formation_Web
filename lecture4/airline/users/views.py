01d2ba9d415bc96a97a0fef6f08dc6a0015ed9c2
GoutteDooo <goudal.donovan@live.fr>
2025-05-16 11:48:10 +0200

    2025-05-16 11:48:10

--- lecture4/airline/users/views.py
+++ lecture4/airline/users/views.py
@@ -13,6 +13,13 @@ def login_view(request):
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return HttpResponseRedirect(reverse("users:index"))
+    else:
+      return render(request, "users/login.html", {
+        "message": "Invalid credentials."
+      })
   return render(request, "users/login.html")
 
 def logout_view(request):
