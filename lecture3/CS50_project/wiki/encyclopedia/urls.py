6d0e2f729ca9003c91e554964905dd55d1c923fd
GoutteDooo <goudal.donovan@live.fr>
2025-05-20 10:00:44 +0200

    2025-05-20 10:00:44

--- lecture3/CS50_project/wiki/encyclopedia/urls.py
+++ lecture3/CS50_project/wiki/encyclopedia/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("new/", views.new, name="new"),
     path("new/save/", views.save_new, name="save_new"),
-    path("edit/<str:title>", views.edit, name="edit"),
-    path("edit/save/", views.save_edit, name="save_edit")
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
+    path("wiki/<str:title>/edit/save/", views.save_edit, name="save_edit")
 ]
