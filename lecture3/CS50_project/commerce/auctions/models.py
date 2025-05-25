c2e5edc1bc1d56be1d8fec85238bf27dd359da65
gouttedooo <goudal.donovan@live.fr>
2025-05-25 17:37:27 +0200

    2025-05-25 17:37:27

--- lecture3/CS50_project/commerce/auctions/models.py
+++ lecture3/CS50_project/commerce/auctions/models.py
@@ -9,9 +9,13 @@ class Listing(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     picture_url = models.URLField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     bids_count = models.IntegerField(default=0)
     last_bid_id = models.ForeignKey("Bid", on_delete=models.SET_NULL, null=True)
+
+class Bid(models.Model):
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
