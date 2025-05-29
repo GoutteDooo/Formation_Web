4a288590e485fdc2c5ce6e2b491b89eada75aa80
GoutteDooo <goudal.donovan@live.fr>
2025-05-29 12:20:30 +0200

    2025-05-29 12:20:30

--- lecture3/CS50_project/commerce/auctions/models.py
+++ lecture3/CS50_project/commerce/auctions/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
 
+    def __str__(self):
+        return self.username
+
 class Listing(models.Model):
     id = models.AutoField(primary_key=True)
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
