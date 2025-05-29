1577e6031fb78a2439e071fc464fcc984ec7cb71
GoutteDooo <goudal.donovan@live.fr>
2025-05-29 22:18:15 +0200

    2025-05-29 22:18:15

--- lecture3/CS50_project/commerce/commerce/settings.py
+++ lecture3/CS50_project/commerce/commerce/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'auctions.User'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
