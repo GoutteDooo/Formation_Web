9ab236b9a5d01ba2241d04d5a0004881ccb9d653
GoutteDooo <goudal.donovan@live.fr>
2025-05-31 15:11:59 +0200

    2025-05-31 15:11:59

--- lecture3/CS50_project/commerce/auctions/models.py
+++ lecture3/CS50_project/commerce/auctions/models.py
@@ -25,8 +25,8 @@ class Listing(models.Model):
     is_active = models.BooleanField(default=True)
     winner_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-    # def __str__(self):
-    #     return f"{self.name}: ${self.initial_price} - end at: {self.end_at.strftime('%Y-%m-%d %H:%M:%S')}"
+    def __str__(self):
+        return f"{self.name}: ${self.initial_price} - end at: {self.end_at.strftime('%Y-%m-%d %H:%M:%S')}"
 
     def get_min_bid_amount(self):
         """Return the minimum bid amount (either last bid amount or initial price)"""
@@ -39,7 +39,7 @@ class Bid(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
 
     def __str__(self):
-        return f"Bid ${self.amount} by {self.user_id.username} on {self.listing_id.name}"
+        return f"{self.id}: Bid ${self.amount} by {self.user_id.username} on {self.listing_id.name}"
 
 class ListingComment(models.Model):
     id = models.AutoField(primary_key=True)
