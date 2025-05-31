de0e7a09fd40dfee7424839cf3e5555f762ef41c
GoutteDooo <goudal.donovan@live.fr>
2025-05-31 17:31:30 +0200

    2025-05-31 17:31:30

--- lecture3/CS50_project/commerce/auctions/admin.py
+++ lecture3/CS50_project/commerce/auctions/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import Listing, Bid, ListingComment
 
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(ListingComment)
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner_id', 'initial_price', 'end_at', 'is_active', 'winner_id')
+    list_filter = ('is_active', 'category')
+    search_fields = ('name', 'owner_id__username')
+    ordering = ('-created_at',)
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('listing_id', 'user_id', 'amount')
+    list_filter = ('listing_id', 'user_id')
+    search_fields = ('listing_id__name', 'user_id__username')
+    ordering = ('-amount',)
+
+@admin.register(ListingComment)
+class ListingCommentAdmin(admin.ModelAdmin):
+    list_display = ('listing_id', 'user_id', 'content')
+    list_filter = ('listing_id', 'user_id')
+    search_fields = ('listing_id__name', 'user_id__username')
+    ordering = ('-created_at',)
