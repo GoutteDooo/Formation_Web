acb83752dce1f8b9e41bb2be4ece6c3f7a7f5074
GoutteDooo <goudal.donovan@live.fr>
2025-05-31 15:26:59 +0200

    2025-05-31 15:26:59

--- lecture3/CS50_project/commerce/auctions/admin.py
+++ lecture3/CS50_project/commerce/auctions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Listing, Bid
+from .models import Listing, Bid, ListingComment
 
 admin.site.register(Listing)
 admin.site.register(Bid)
