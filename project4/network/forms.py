0f6433552fc444da0dfba0d4ef5ff6a4768c43d1
GoutteDooo <goudal.donovan@live.fr>
2025-06-04 11:41:04 +0200

    2025-06-04 11:41:04

--- project4/network/forms.py
+++ project4/network/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
 class PostForm(forms.Form):
-    content = forms.CharField(label="New post", max_length=1000, widget=forms.Textarea)
+    content = forms.CharField(
+        label="New post",
+        max_length=1000,
+        widget=forms.Textarea(attrs={
+            'placeholder': 'How are you feeling today?',
+            'rows': 6,  # Optional: control height
+            'cols': 50  # Optional: control width
+        })
+    )
