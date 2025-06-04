ff259ef742cb6e4b16acbef5073879801c064984
GoutteDooo <goudal.donovan@live.fr>
2025-06-04 13:42:32 +0200

    2025-06-04 13:42:32

--- project4/network/static/network/app.js
+++ project4/network/static/network/app.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const submitPost = document.querySelector("#new-post__form");
 
-
   submitPost.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch("new_post/", () => {
-      
-    }
+    send_post(e);
   })
 })
+
+async function send_post(datas) {
+    datas.preventDefault();
+  const datas = datas;
+  try {
+    const res = await fetch("new_post/", {
+      method:"POST",
+    });
+    console.log(datas);
+  }
+  catch (err) {}
+}
