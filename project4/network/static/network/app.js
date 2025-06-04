23dc831f3f88e6bff1025415f3e2eb4e7b465572
GoutteDooo <goudal.donovan@live.fr>
2025-06-04 13:57:34 +0200

    2025-06-04 13:57:34

--- project4/network/static/network/app.js
+++ project4/network/static/network/app.js
@@ -1,27 +1,28 @@
+let form;
 document.addEventListener('DOMContentLoaded', () => {
-  const submitPost = document.querySelector("#new-post__form");
+  form = document.querySelector("#new-post__form");
 
-  submitPost.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     send_post(e);
   })
 })
 
 async function send_post(datas) {
+  const url = form.dataset.url;
   const content = datas.target[1].value;
   try {
-    const res = await fetch("new_post/", {
+    const res = await fetch(url, {
       method:"POST",
       headers:{
         "Content-Type":"application/json"
+        ""
       },
       body: JSON.stringify({content})
     });
     // console.log(await res.json());
-    
   }
   catch (err) {
     console.error("unexpected error when sending post:",err);
-    
   }
 }
