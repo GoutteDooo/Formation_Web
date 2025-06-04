5192f83e04630e7752c542ce1d42330578428b94
GoutteDooo <goudal.donovan@live.fr>
2025-06-04 14:06:34 +0200

    2025-06-04 14:06:34

--- project4/network/static/network/app.js
+++ project4/network/static/network/app.js
@@ -11,13 +11,15 @@ document.addEventListener('DOMContentLoaded', () => {
 async function send_post(e) {
   const url = form.dataset.url;
   const content = e.target[1].value;
+  const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
   const data = new FormData();
   data.append('content', content);
+
   try {
     const res = await fetch(url, {
       method:"POST",
-      headers: { "X-CSRFToken": '{{csrf_token}}' },
+      headers: { "X-CSRFToken": csrfToken },
       body: data,
     });
     // console.log(await res.json());
