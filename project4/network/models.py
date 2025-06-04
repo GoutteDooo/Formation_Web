38f557109f7f39ed7768d8e2686010eb70dcfe98
GoutteDooo <goudal.donovan@live.fr>
2025-06-04 14:49:04 +0200

    2025-06-04 14:49:04

--- project4/network/models.py
+++ project4/network/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Post:
+class Post(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
     content = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
