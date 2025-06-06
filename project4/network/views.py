a316159393d15051dd62f3a0256050eac3cf336a
GoutteDooo <goudal.donovan@live.fr>
2025-06-06 09:47:41 +0200

    2025-06-06 09:47:41

--- project4/network/views.py
+++ project4/network/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import User, Post, FollowModel
 from .forms import PostForm
