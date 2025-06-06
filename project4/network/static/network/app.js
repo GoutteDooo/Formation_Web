d7a91c2d4feea8c3a8f5bcc5d11390280d115398
GoutteDooo <goudal.donovan@live.fr>
2025-06-06 10:53:41 +0200

    2025-06-06 10:53:41

--- project4/network/static/network/app.js
+++ project4/network/static/network/app.js
@@ -183,9 +183,9 @@ function generate_posts(pageType, posts_data) {
 
 const edit_post = (post) => {
   console.log(post);
-  const editButton = post.querySelector(".post-edit"),
+  const editButton = post.getElementByClassname(".post-edit"),
     editBtnClone = editButton.cloneNode(true);
-    editButton.parentNode
+    console.dir(editButton);
     editBtnClone.textContent = "Save";
   const content = post.querySelector(".post-text");
   
