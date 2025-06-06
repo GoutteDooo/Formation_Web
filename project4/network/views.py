e18882e9c4f5252180287581ca937f1a8f928d28
GoutteDooo <goudal.donovan@live.fr>
2025-06-06 11:35:42 +0200

    2025-06-06 11:35:42

--- project4/network/views.py
+++ project4/network/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 
 from .models import User, Post, FollowModel
